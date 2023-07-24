e1538646ab1987cc5f7197342a406cf4cdd16abc
Basnet4567 <it212202@tridenta.ac.jp>
2023-07-25 00:11:27 +0900

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println();
+
+                int abd=2;
+        System.out.println(abd);
+
+    }
+}
+q
